63850efa2bf7c503c90265f605474434a3a3789e
shm-dsgn <77527904+shm-dsgn@users.noreply.github.com>
2023-06-17 04:52:38 +0530

    Initial commit with user route done

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+//enables the use of environment variables
+import dotenv from "dotenv";
+dotenv.config();
+
+import { userRouter } from "./routes/user.js";
+// import { recipesRouter } from "./routes/recipes.js";
+
+const app = express();
+
+//coverts all incoming request to json
+app.use(express.json());
+//CORS supports secure cross-origin requests and data transfers between browsers and servers
+app.use(cors());
+
+app.use("/auth", userRouter);
+// app.use("/recipes", recipesRouter);
+
+//connect to mongodb
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+
+mongoose.connect(
+  `mongodb+srv://${dbUser}:${dbPassword}@recipeapp.omfhe5h.mongodb.net/recipeapp?retryWrites=true&w=majority`);
+
+app.listen(3001, () => console.log("Server running on port 3001"));
---
+++ src/routes/user.js
@@ -0,0 +1,47 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import { UserModel } from "../models/Users.js";
+
+dotenv.config();
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await UserModel.findOne({ username: username });
+
+  if (user) {
+    return res.json({ message: "User already exists" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new UserModel({
+    username: username,
+    password: hashedPassword,
+  });
+  await newUser.save();
+
+  res.json({ message: "User registered successfully" });
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await UserModel.findOne({ username: username });
+
+  if (!user) {
+    return res.json({ message: "User not found" });
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordCorrect) {
+    res.json({ message: "Invalid credentials" });
+  }
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  res.json({ token: token, userID : user._id });
+});
+
+export { router as userRouter };
